04054621a62586ed1d52a0d58172c10420c82886
TonyVale <97236366+TonyVale@users.noreply.github.com>
2023-08-01 09:56:00 -0300

    Update Clase1.c

--- Clase1.c
+++ Clase1.c
@@ -1,1 +1,26 @@
+//C es un lenguaje compilado
+//librerías
 
+#include <stdio.h>
+
+//Function Examples
+//Functions before main. If main is first, the function will by defined as "int function (int a )" so i will have warning and lost data if the original function isn´t like that. 
+//Puedo evitar el error, declarando la función antes del main
+
+double dobleFunction(double a);
+
+int intFunction(int a){
+  return a;
+}
+
+//main function
+
+int main(void){
+  
+  printf("HelloWorld");
+  return 0;
+}
+
+double doubleFunction(double a){
+  ...
+}
