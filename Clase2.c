bbdb6313c19c233b69443c4e5aaaf1cf4d9c2f83
TonyVale <97236366+TonyVale@users.noreply.github.com>
2023-08-04 14:42:41 -0300

    Update Clase2.c

--- Clase2.c
+++ Clase2.c
@@ -4,9 +4,13 @@
 
 //Enteros
 
-unsigned char U_char; // 1 byte [0 , 255 
-signed char S_char; // 1 byte [-128 , 127] (Complemento a la base)
+unsigned char U_char;  // 1 byte [0 , 255 
+signed char S_char;    // 1 byte [-128 , 127] (Complemento a la base)
 
-unsigned int U_int; // 2 bytes [0, 65535] ( short ) o 4 bytes ( long )
-signed bytes S_int; // 2 bytes [-32768 , 32767] ( short ) o 4 bytes ( long )
+unsigned int U_int;    // 2 bytes [0, 65535] ( short ) o 4 bytes ( long )
+signed bytes S_int;    // 2 bytes [-32768 , 32767] ( short ) o 4 bytes ( long )
 
+//punto flotante
+
+float  f; // 32 bits, 4 bytes 
+double d; // 64 bits, 8 bytes
