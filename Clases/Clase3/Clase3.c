88aa1f85a507821bde5a04555d20c9b6e8e33065
TonyVale <97236366+TonyVale@users.noreply.github.com>
2023-08-07 10:32:00 -0300

    Create Clase3.c

---
+++ Clases/Clase3/Clase3.c
@@ -0,0 +1,12 @@
+#include  <stdio.h>
+
+#define  MACRO  18
+
+//Constantes Martes = LUNES+1, MIERCOLES=MARTES+1, etc
+
+enum  dias  { LUNES=0 , MARTES, MIERCOLES, JUEVES, VIERNES, SAVADO, DOMINGO };
+enum  carreras  { INF="A", KEL="K", LOK="Q" }
+
+//Atributos de las variables
+//Tipo de dato, Rotulo, Valor Izquierdo (Dirección de memoria), Valor Derecho (Valor real de la variable), Nombre.
+
