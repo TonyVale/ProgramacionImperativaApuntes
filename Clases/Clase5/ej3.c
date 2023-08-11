72881845ef50a9ba092097e70c1b6aaa765f6dae
TonyVale <97236366+TonyVale@users.noreply.github.com>
2023-08-11 15:57:25 -0300

    Create ej3.c

---
+++ Clases/Clase5/ej3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "getnum.h"
+
+int main(){
+  
+  unsigned int num, aux;
+
+  num = getint("Ingrese su numero:");
+  
+  aux = num <= 15 ? (num > 10 ? num + "0" : num - 10 + "A" ) : num );
+  printf("%d", aux);
+  
+  return 0;
+}
