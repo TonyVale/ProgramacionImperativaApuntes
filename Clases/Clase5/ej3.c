725b8b7a303c7506779e50bac25d6f75ff442814
TonyVale <97236366+TonyVale@users.noreply.github.com>
2023-08-11 15:58:41 -0300

    Update ej3.c

--- Clases/Clase5/ej3.c
+++ Clases/Clase5/ej3.c
@@ -7,7 +7,7 @@ int main(){
 
   num = getint("Ingrese su numero:");
   
-  aux = num <= 15 ? (num > 10 ? num + "0" : num - 10 + "A" ) : num );
+  aux = num <= 15 ? (num > 10 ? num + "0" : num - 10 + "A" ) : num ;
   printf("%d", aux);
   
   return 0;
