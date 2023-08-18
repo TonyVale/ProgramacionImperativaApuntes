ee968b8878da3c9a7532dd6378a5847fce2c1e70
TonyVale <97236366+TonyVale@users.noreply.github.com>
2023-08-18 15:00:15 -0300

    Update ej2.c

--- Clases/Clase8/ej2.c
+++ Clases/Clase8/ej2.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include "getnum.h"
 
-int main(void){oo
+int main(void){
+  
+  float num=getfloat("");
+  int aux, suma;
+  suma=aux=0;
 
-
+  do{
+    
+    int entero = num;
+    num -= entero;
+    num+10;
+    aux = (int)num;
+    suma += aux;
+    
+  }while("%d \n", suma)
 }
