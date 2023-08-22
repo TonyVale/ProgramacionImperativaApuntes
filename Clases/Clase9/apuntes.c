90080924a20f25ae1546b74efae4a9c9305085fc
TonyVale <97236366+TonyVale@users.noreply.github.com>
2023-08-22 09:52:19 -0300

    Create apuntes.c

---
+++ Clases/Clase9/apuntes.c
@@ -0,0 +1,36 @@
+//standard input/output librery
+#include <stdio.h>
+
+//add booleans values (true/false)
+#include <stdbool.h>
+
+//abs, rand
+#include <stdlib.h>
+
+//time
+#include <time.h>
+
+//Data types check librery
+#include <ctype.h>
+
+//int getchar(void)
+//int perror(const char * s); Imprime mensajes de error en consola 
+
+int main(){
+  int a;
+  long l;
+
+  //abs value
+  abs(a);
+  labs(b);
+
+  //return mils since 1970 aprox
+  time(NULL);
+  
+  //gen random number
+  rand();
+
+  //gen random number with a seed
+  srand(seed: time(timer: NULL)); 
+  
+}
