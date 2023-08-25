2760311318a6257823c6ea8ff0aa4d66aec36137
TonyVale <97236366+TonyVale@users.noreply.github.com>
2023-08-25 15:51:10 -0300

    Create ej2.c

---
+++ Clases/Clase10/ej2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "./random/random/random.h"
+
+char randLetra(void);
+
+int main(){
+  randomize();
+  printf("%c %d", randLetra() , randInt(1 , 256))
+  return 0;
+}
+
+char randLetra(void){
+  return(randInt(0, 27) + 'A');
+}
