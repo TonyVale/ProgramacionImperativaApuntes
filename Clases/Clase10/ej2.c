8648d71f1187fe8be5acc3889d79228f1da8f02d
TonyVale <97236366+TonyVale@users.noreply.github.com>
2023-08-25 16:04:42 -0300

    Update ej2.c

--- Clases/Clase10/ej2.c
+++ Clases/Clase10/ej2.c
@@ -2,13 +2,33 @@
 #include "./random/random/random.h"
 
 char randLetra(void);
+int read(char)
 
 int main(){
   randomize();
-  printf("%c %d", randLetra(), randInt(1 , 256));
+  
+  char l;
+  int n; 
+
+  l = randLetra();
+  n = randInt(1 , 256);
+    
+  printf("%c %d", l , n );
+
+  printf("%d", read(l));
+  
   return 0;
 }
 
 char randLetra(void){
   return(randInt(0, 25) + 'A');
+}
+
+int read(char c){
+  int cont = 0;
+  while ((char aux = getchar()) != EOF){
+    if ( aux == c)
+      ++cont;
+  }
+  return cont;
 }
